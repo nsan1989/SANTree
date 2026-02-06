5a93c977701f05b05599cfbd22ba6ae62c8d1f08
nsan1989 <nsanajaoba@yahoo.com>
2026-02-06 15:42:48 +0530

    ssms_v.5.5.f

--- san_tree/san_tree/settings.py
+++ san_tree/san_tree/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+d7+e549uuue8$h(sl9o_tn*7cpwdye7-dbpt1lji5i*3*6hfz'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
