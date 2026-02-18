ceeda70fa596dd8551cb748fe973bfedcfd8596a
nsan1989 <nsanajaoba@yahoo.com>
2026-02-18 14:06:21 +0530

    ssms_v.5.5.m

--- san_tree/san_cms/views.py
+++ san_tree/san_cms/views.py
@@ -701,10 +701,7 @@ def AssignedTasks(request):
         user_role = user.role
     except:
         raise PermissionDenied("User profile not found.")
-    assign_complaints = ComplaintHistory.objects.filter(
-        Q(complaint__assigned_to = user) &
-        Q(Q(complaint__status = 'Review') | Q(complaint__status = 'Resolved'))
-        ).order_by('complaint__created_at')
+    assign_complaints = ComplaintHistory.objects.filter(complaint__assigned_to = user).order_by('complaint__created_at')
 
     page_number = request.GET.get('page')
     paginator = Paginator(assign_complaints, 10)  
